b53e73cf3b539ab693eba582bd08bc8aab15ae48
EmmanuellDev <emmanuelsk04@gmail.com>
2024-09-05 16:34:15 +0530

    Herbal Garden

--- README.md
+++ README.md
@@ -68,3 +68,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+Dependencies:
+* npm install three @react-three/fiber @react-three/drei
+* npm install react-router-dom
+* npm install @mui/material @emotion/react @emotion/styled
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React and necessary components
+import React from 'react';
+import HerbalGarden from './HerbalGarden'; // Adjust the path if needed
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Main App Container */}
+      <HerbalGarden />
     </div>
   );
 }
